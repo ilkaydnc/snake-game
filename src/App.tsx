9d59f6069b3fa9e6040d111e3c66b79320ac3e1f
ilkaydnc <ilkaydinc1@gmail.com>
2021-03-23 13:42:45 +0300

    Create initial snake

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import { useEffect, useState } from "react";
+import cs from "classnames";
 import "./App.css";
 
 const MAP_WIDTH = 32;
 const MAP_HEIGHT = 32;
+const DEFAULT_SNAKE_SIZE = 3;
 
 const createNewMap = (width: number, height: number): string[] => {
   return new Array(width * height).fill("O");
 };
 
+const createSnake = (size: number): number[] => {
+  return new Array(size).fill(0).map((_, i) => i);
+};
+
 function App() {
   const [map, setMap] = useState<string[] | undefined>(undefined);
+  const [snake, setSnake] = useState<number[] | undefined>([]);
 
   useEffect(() => {
     const emptyMap = createNewMap(MAP_WIDTH, MAP_HEIGHT);
+    const newSnake = createSnake(DEFAULT_SNAKE_SIZE);
+
+    newSnake.forEach((value) => {
+      emptyMap[value] = "S";
+    });
 
     setMap(emptyMap);
+    setSnake(newSnake);
   }, []);
   return (
     <div className="App">
@@ -28,7 +41,7 @@ function App() {
         {map?.map((cell, index) => (
           <div
             key={index}
-            className="Map__cell"
+            className={cs("Map__cell", cell === "S" && "Map__snake")}
             onClick={() => console.log(index)}
           />
         ))}
