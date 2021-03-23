5da25e3e4565ea80921ded6916172ac7ce0455f3
ilkaydnc <ilkaydinc1@gmail.com>
2021-03-23 13:32:46 +0300

    Create map

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+const MAP_WIDTH = 32;
+const MAP_HEIGHT = 32;
+
+const createNewMap = (width: number, height: number): string[] => {
+  return new Array(width * height).fill("O");
+};
+
 function App() {
-  return <div className="App"></div>;
+  const [map, setMap] = useState<string[] | undefined>(undefined);
+
+  useEffect(() => {
+    const emptyMap = createNewMap(MAP_WIDTH, MAP_HEIGHT);
+
+    setMap(emptyMap);
+  }, []);
+  return (
+    <div className="App">
+      <div
+        className="Map"
+        style={{
+          gridTemplateColumns: `repeat(${MAP_WIDTH}, 24px)`,
+          gridAutoRows: `repeat(${MAP_HEIGHT}, 24px)`,
+        }}
+      >
+        {map?.map((cell, index) => (
+          <div
+            key={index}
+            className="Map__cell"
+            onClick={() => console.log(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
