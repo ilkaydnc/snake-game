135c1a1ed04eca451c70c168ae6a279d704c4933
ilkaydnc <ilkaydinc1@gmail.com>
2021-03-23 16:19:16 +0300

    Add keyboard control and score state

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ import "./App.css";
 type Direction = "R" | "L" | "U" | "D";
 type SnakePosition = number[];
 
-const MAP_WIDTH = 32;
-const MAP_HEIGHT = 32;
-const DEFAULT_SNAKE_SIZE = 3;
-let GAME_LOOP = 300;
-
 const createNewMap = (width: number, height: number): string[] => {
   return new Array(width * height).fill("O");
 };
@@ -53,16 +48,26 @@ const getNextPosition = (
   }
 };
 
+const MAP_WIDTH = 32;
+const MAP_HEIGHT = 32;
+const DEFAULT_SNAKE_SIZE = 3;
+let GAME_LOOP = 50;
+let direction: Direction = "R";
+let interval: any;
+
 function App() {
+  const [score, setScore] = useState<number>(0);
   const [map, setMap] = useState<string[] | undefined>(undefined);
   const [snake, setSnake] = useState<SnakePosition>([]);
-  const [direction, setDirection] = useState<Direction>("R");
   const [gameOver, setGameOver] = useState<boolean>(false);
 
-  useEffect(() => {
+  const createNewGame = () => {
     const initialMap = createNewMap(MAP_WIDTH, MAP_HEIGHT);
     const newSnake = createSnake(DEFAULT_SNAKE_SIZE);
     const newAppleIndex = createRandomApple(MAP_WIDTH, MAP_HEIGHT, newSnake);
+
+    direction = "R";
+    clearInterval(interval);
 
     newSnake.forEach((value) => {
       initialMap[value] = "S";
@@ -70,76 +75,117 @@ function App() {
 
     initialMap[newAppleIndex] = "X";
 
+    setScore(0);
     setMap(initialMap);
     setSnake(newSnake);
+    setGameOver(false);
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if ((e.key === "ArrowUp" || e.key === "KeyW") && direction !== "D") {
+      direction = "U";
+    } else if (
+      (e.key === "ArrowDown" || e.key === "KeyS") &&
+      direction !== "U"
+    ) {
+      direction = "D";
+    } else if (
+      (e.key === "ArrowLeft" || e.key === "KeyA") &&
+      direction !== "R"
+    ) {
+      direction = "L";
+    } else if (
+      (e.key === "ArrowRight" || e.key === "KeyD") &&
+      direction !== "L"
+    ) {
+      direction = "R";
+    }
+  };
+
+  const moveSnake = () => {
+    if (!map && !snake) return null;
+
+    const snakeHead = snake[snake.length - 1];
+    const nextPosition = getNextPosition(snakeHead, MAP_WIDTH, direction);
+
+    if (
+      // Left Wall
+      (snakeHead % MAP_WIDTH === 0 && direction === "L") ||
+      // Right Wall
+      (snakeHead % MAP_WIDTH === MAP_WIDTH - 1 && direction === "R") ||
+      // Top Wall
+      nextPosition < 0 ||
+      // Bottom Wall
+      nextPosition > MAP_WIDTH * MAP_HEIGHT ||
+      // Snake Collision
+      snake.includes(nextPosition)
+    ) {
+      clearInterval(interval);
+      setGameOver(true);
+
+      return null;
+    }
+
+    const cloneSnake = Array.from(snake);
+    const cloneMap = Array.from(map || []);
+
+    cloneSnake.push(nextPosition);
+
+    if (cloneMap[nextPosition] === "X") {
+      cloneMap[nextPosition] = "O";
+
+      const newAppleIndex = createRandomApple(
+        MAP_WIDTH,
+        MAP_HEIGHT,
+        cloneSnake
+      );
+
+      cloneMap[newAppleIndex] = "X";
+
+      setScore((prevScore) => prevScore + 1);
+
+      setSnake(cloneSnake);
+      setMap(cloneMap);
+
+      return null;
+    }
+
+    cloneSnake.shift();
+
+    snake.forEach((value) => {
+      cloneMap[value] = "O";
+    });
+
+    cloneSnake.forEach((value) => {
+      cloneMap[value] = "S";
+    });
+
+    setSnake(cloneSnake);
+    setMap(cloneMap);
+  };
+
+  useEffect(() => {
+    createNewGame();
   }, []);
 
   useEffect(() => {
-    let interval: any;
-
     if (!gameOver && map && snake) {
-      interval = setInterval(() => {
-        const snakeHead = snake[snake.length - 1];
-        const nextPosition = getNextPosition(snakeHead, MAP_WIDTH, direction);
-
-        if (
-          // Left Wall
-          (snakeHead % MAP_WIDTH === 0 && direction === "L") ||
-          // Right Wall
-          (snakeHead % MAP_WIDTH === MAP_WIDTH - 1 && direction === "R") ||
-          // Top Wall
-          nextPosition < 0 ||
-          // Bottom Wall
-          nextPosition > MAP_WIDTH * MAP_HEIGHT ||
-          // Snake Collision
-          snake.includes(nextPosition)
-        ) {
-          clearInterval(interval);
-          setGameOver(true);
-
-          return null;
-        }
-
-        const cloneSnake = Array.from(snake);
-        const cloneMap = Array.from(map);
-
-        cloneSnake.push(nextPosition);
-
-        if (map[nextPosition] === "X") {
-          cloneMap[nextPosition] = "O";
-
-          const newAppleIndex = createRandomApple(
-            MAP_WIDTH,
-            MAP_HEIGHT,
-            cloneSnake
-          );
-
-          cloneMap[newAppleIndex] = "X";
-
-          setSnake(cloneSnake);
-          setMap(cloneMap);
-
-          return null;
-        }
-
-        cloneSnake.shift();
-
-        snake.forEach((value) => {
-          cloneMap[value] = "O";
-        });
-
-        cloneSnake.forEach((value) => {
-          cloneMap[value] = "S";
-        });
-
-        setSnake(cloneSnake);
-        setMap(cloneMap);
-      }, GAME_LOOP);
+      interval = setInterval(moveSnake, GAME_LOOP);
     } else {
       clearInterval(interval);
     }
 
     return () => interval && clearInterval(interval);
+  }, [map, snake, gameOver]);
+
+  useEffect(() => {
+    if (map && snake && !gameOver) {
+      window.addEventListener("keydown", onKeyDown);
+    } else {
+      window.removeEventListener("keydown", onKeyDown);
+    }
+
+    return () => window.removeEventListener("keydown", onKeyDown);
   }, [map, snake, gameOver]);
 
   return (
